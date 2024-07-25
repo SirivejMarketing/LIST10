8c1cfd92f4a9b346cbf1f0b6ec120859977f17db
ThunPao <147371648+SirivejMarketing@users.noreply.github.com>
2024-07-25 16:46:32 +0700

    Create servessr.cjs

---
+++ bootstrap/servessr.cjs
@@ -0,0 +1,27 @@
+const polka = require("polka");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = polka();
+import("./bootstrap/ssr/ssr.js")
+    .then(({ handler }) => {
+        if (typeof handler === "function") {
+            app.use(handler);
+            console.log(
+                "Inertia SSR handler successfully loaded and attached."
+            );
+        } else {
+            console.error("Imported handler is not a function:", handler);
+        }
+    })
+    .catch((err) => {
+        console.error("Failed to import handler:", err);
+        process.exit(1);
+    });
+
+app.get("/healthcheck", (req, res) => {
+    res.end("ok");
+});
+// async function loadApp() {
+//     await import("./bootstrap/ssr/ssr.js");
+// }
+// loadApp();
